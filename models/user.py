acb7299aab12153b8cdc8307971faf76be7d01c2
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-07-30 11:10:57 +0300

    added profile pic support

--- models/user.py
+++ models/user.py
@@ -1,14 +1,11 @@
 from utils.database import Database
-from bson.objectid import ObjectId
 
 class UserModel:
     def __init__(self):
         self.collection = Database('todo_app').get_collection('users')
 
-    def create_user(self, username, password, email=None):
-        user = {"username": username, "password": password}
-        if email:
-            user['email'] = email
+    def create_user(self, username, password, profile_picture=None):
+        user = {"username": username, "password": password, "profile_picture": profile_picture}
         self.collection.insert_one(user)
 
     def find_user(self, username):
@@ -18,14 +15,14 @@ class UserModel:
         user = self.find_user(username)
         return user and user['password'] == password
 
+    def update_profile(self, username, email=None, profile_picture=None):
+        update_fields = {}
+        if email:
+            update_fields['email'] = email
+        if profile_picture:
+            update_fields['profile_picture'] = profile_picture
+        if update_fields:
+            self.collection.update_one({"username": username}, {"$set": update_fields})
+
     def get_all_users(self):
         return self.collection.find()
-
-    def update_user(self, user_id, username, password, email=None):
-        update_data = {"username": username, "password": password}
-        if email:
-            update_data['email'] = email
-        self.collection.update_one({"_id": ObjectId(user_id)}, {"$set": update_data})
-
-    def delete_user(self, user_id):
-        self.collection.delete_one({"_id": ObjectId(user_id)})
