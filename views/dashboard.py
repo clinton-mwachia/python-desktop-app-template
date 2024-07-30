078dea552609e746b8a49ff965980aff42e1fa13
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-07-30 11:09:56 +0300

    added logout button to dashboard

--- views/dashboard.py
+++ views/dashboard.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 from views.todo import TodoView
 from views.user import UserView
+from views.profile import ProfileView
 
 class DashboardView:
-    def __init__(self, sidebar_frame, content_frame, username):
+    def __init__(self, sidebar_frame, content_frame, username, logout_callback):
         self.sidebar_frame = sidebar_frame
         self.content_frame = content_frame
         self.username = username
+        self.logout_callback = logout_callback
 
         self.setup_sidebar()
 
@@ -17,10 +19,13 @@ class DashboardView:
         tk.Button(self.sidebar_frame, text="Dashboard", command=self.show_dashboard).pack(fill=tk.X)
         tk.Button(self.sidebar_frame, text="Todos", command=self.show_todos).pack(fill=tk.X)
         tk.Button(self.sidebar_frame, text="Users", command=self.show_users).pack(fill=tk.X)
+        tk.Button(self.sidebar_frame, text="Profile", command=self.show_profile).pack(fill=tk.X)
+        tk.Button(self.sidebar_frame, text="Logout", command=self.logout).pack(fill=tk.X)
 
     def show_dashboard(self):
         self.clear_content()
         # Add any dashboard-specific widgets here
+        tk.Label(self.content_frame, text="Welcome to the Dashboard").pack(pady=20)
 
     def show_todos(self):
         self.clear_content()
@@ -29,6 +34,14 @@ class DashboardView:
     def show_users(self):
         self.clear_content()
         UserView(self.content_frame)
+
+    def show_profile(self):
+        self.clear_content()
+        ProfileView(self.content_frame, self.username)
+
+    def logout(self):
+        self.clear_content()
+        self.logout_callback()  # Call the logout callback function
 
     def clear_content(self):
         for widget in self.content_frame.winfo_children():
