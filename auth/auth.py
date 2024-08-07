e218ed871268209192bb8b27fc8935e5825d1dda
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-08-07 12:47:47 +0300

    added email when registering a user

--- auth/auth.py
+++ auth/auth.py
@@ -4,10 +4,10 @@ class AuthController:
     def __init__(self):
         self.user_model = UserModel()
 
-    def register(self, username, password):
+    def register(self, username, password, email):
         if self.user_model.find_user(username):
             return False
-        self.user_model.create_user(username, password)
+        self.user_model.create_user(username, password, email)
         return True
 
     def login(self, username, password):
