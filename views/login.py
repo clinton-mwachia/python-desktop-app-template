1bea1e2c94062c4eee94bcb2048be636a3121b10
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-08-07 12:44:56 +0300

    added register and dashboard callbacks

--- views/login.py
+++ views/login.py
@@ -7,15 +7,16 @@ import logging
 logger = logging.getLogger("application_logger")
 
 class LoginView:
-    def __init__(self, root, on_login_success):
+    def __init__(self, root, show_dashboard_callback, show_register_callback):
         self.root = root
         self.root.title("TEMPLATE") # title of the main frame
         self.root.geometry("400x400") # width and height of the main frame
-        self.on_login_success = on_login_success
+        self.show_dashboard_callback = show_dashboard_callback
+        self.show_register_callback = show_register_callback
         self.auth_controller = AuthController()
 
         self.frame = tk.Frame(root, bg='white', borderwidth=2, relief='groove', padx=20, pady=20)
-        self.frame.place(relx=0.5, rely=0.5, anchor='center', width=300, height=200)
+        self.frame.place(relx=0.5, rely=0.5, anchor='center', width=300, height=300)
 
         tk.Label(self.frame, text="Username").pack()
         self.username_entry = tk.Entry(self.frame)
@@ -28,7 +29,8 @@ class LoginView:
         login_button = tk.Button(self.frame, text="Login", command=self.login)
         login_button.pack(pady=10)
 
-        register_button = tk.Button(self.frame, text="Register", command=self.register)
+        tk.Label(self.frame, text="No account?").pack()
+        register_button = tk.Button(self.frame, text="Register", command=self.show_register_callback)
         register_button.pack()
 
     def login(self):
@@ -36,26 +38,12 @@ class LoginView:
         password = self.password_entry.get()
         if username and password:
             if self.auth_controller.login(username, password):
-                self.on_login_success(username)
+                messagebox.showinfo("Login Success", "User logged in successfull")
                 logger.info(f'{username} logged in')
+                self.show_dashboard_callback(username)
             else:
                 messagebox.showerror("Login Failed", "Invalid username or password")
                 logger.error(f'{username}: Invalid username or password')
         else:
             messagebox.showerror("Login Failed", "Please enter username and password")
             logger.error('Please enter username and password')
-
-    def register(self):
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-        
-        if username and password:
-            if self.auth_controller.register(username, password):
-                messagebox.showinfo("Registration Successful", "You can now log in")
-                logger.info(f'{username} registered successfully')
-            else:
-                messagebox.showerror("Registration Failed", "Username already exists")
-                logger.error(f'{username}: already exists')
-        else:
-            messagebox.showerror("Registration Failed", "please provide the Username and password")
-            logger.error('please provide the Username and password')
